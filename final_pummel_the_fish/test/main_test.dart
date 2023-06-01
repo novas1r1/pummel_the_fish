0a020fa588980f65f829249a3afeaa81e7fc642e
Verena Zaiser <development@verena-zaiser.de>
2023-06-01 13:40:38 +0200

    added tests

---
+++ final_pummel_the_fish/test/main_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('main ...', (tester) async {
+    // TODO: Implement test
+  });
+}
---
+++ final_pummel_the_fish/test/screens/edit_pet_screen_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('edit pet screen ...', (tester) async {
+    // TODO: Implement test
+  });
+}
