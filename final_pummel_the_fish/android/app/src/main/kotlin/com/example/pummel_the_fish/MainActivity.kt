e6a89e46b599ea01f2417ca278b37cbee313ca9f
Verena Zaiser <development@verena-zaiser.de>
2023-06-01 13:40:38 +0200

    added tests

---
+++ final_pummel_the_fish/android/app/src/main/kotlin/com/example/pummel_the_fish/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pummel_the_fish
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ final_pummel_the_fish/lib/main.dart
@@ -0,0 +1,122 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pummel_the_fish/data/repositories/firestore_pet_repository.dart';
+import 'package:pummel_the_fish/firebase_options.dart';
+import 'package:pummel_the_fish/screens/create_pet_screen.dart';
+import 'package:pummel_the_fish/screens/home_screen.dart';
+import 'package:pummel_the_fish/screens/splash_screen.dart';
+import 'package:pummel_the_fish/theme/custom_colors.dart';
+import 'package:pummel_the_fish/widgets/adoption_bag_wrapper.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AdoptionBagWrapper(
+      child: RepositoryProvider(
+        create: (context) => FirestorePetRepository(
+          firestore: FirebaseFirestore.instance,
+        ),
+        child: MaterialApp(
+          title: "Pummel The Fish",
+          theme: ThemeData(
+            useMaterial3: true,
+            colorScheme: const ColorScheme(
+              brightness: Brightness.light,
+              primary: CustomColors.blueDark,
+              onPrimary: CustomColors.white,
+              secondary: CustomColors.orange,
+              onSecondary: CustomColors.white,
+              error: CustomColors.red,
+              onError: CustomColors.white,
+              background: CustomColors.white,
+              onBackground: CustomColors.blueMedium,
+              surface: CustomColors.blueLight,
+              onSurface: CustomColors.white,
+            ),
+            inputDecorationTheme: const InputDecorationTheme(
+              labelStyle: TextStyle(
+                fontFamily: "Titillium Web",
+                fontWeight: FontWeight.w700,
+                fontSize: 16,
+                color: CustomColors.blueDark,
+              ),
+              floatingLabelStyle: TextStyle(
+                fontFamily: "Titillium Web",
+                fontWeight: FontWeight.w700,
+                fontSize: 12,
+                color: CustomColors.blueDark,
+              ),
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: CustomColors.blueDark),
+              ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: CustomColors.blueLight,
+                ),
+              ),
+              errorBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: CustomColors.red),
+              ),
+              focusedErrorBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: CustomColors.red),
+              ),
+            ),
+            fontFamily: "Comfortaa",
+            textTheme: const TextTheme(
+              headlineLarge: TextStyle(
+                fontFamily: "Titillium Web",
+                fontWeight: FontWeight.w700,
+                fontSize: 20,
+                color: CustomColors.blueDark,
+              ),
+              titleMedium: TextStyle(
+                fontFamily: "Comfortaa",
+                fontWeight: FontWeight.w600,
+                fontSize: 18,
+                color: CustomColors.blueMedium,
+              ),
+              bodyLarge: TextStyle(
+                fontFamily: "Titillium Web",
+                fontWeight: FontWeight.w700,
+                fontSize: 16,
+                color: CustomColors.blueDark,
+              ),
+              bodyMedium: TextStyle(
+                fontFamily: "Titillium Web",
+                fontWeight: FontWeight.w700,
+                fontSize: 14,
+                color: CustomColors.white,
+              ),
+              bodySmall: TextStyle(
+                fontFamily: "Titillium Web",
+                fontWeight: FontWeight.w700,
+                fontSize: 12,
+                color: CustomColors.blueDark,
+              ),
+            ),
+          ),
+          initialRoute: "/",
+          routes: {
+            "/": (context) => const SplashScreen(),
+            "/home": (context) => const HomeScreen(),
+            "/create": (context) => const CreatePetScreen(),
+          },
+        ),
+      ),
+    );
+  }
+}
