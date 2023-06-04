c33b8753540ce21df59bcf7dabb0d97fa26d9f93
Verena Zaiser <development@verena-zaiser.de>
2023-06-04 15:57:13 +0200

    added tests

--- final_pummel_the_fish/test/widgets/custom_button_test.dart
+++ final_pummel_the_fish/test/widgets/custom_button_test.dart
@@ -1,7 +1,23 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:pummel_the_fish/widgets/custom_button.dart';
 
 void main() {
   testWidgets('custom button ...', (tester) async {
-    // TODO: Implement test
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: CustomButton(onPressed: () {}, label: "Test Custom Button"),
+        ),
+      ),
+    );
+
+    expect(find.text("Test Custom Button"), findsOneWidget);
+    expect(find.byType(CustomButton), findsOneWidget);
+
+    await expectLater(
+      find.byType(MaterialApp),
+      matchesGoldenFile("custom_button.png"),
+    );
   });
 }
