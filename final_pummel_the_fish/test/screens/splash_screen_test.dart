5714212f5c8c903d6e6e7fb597c65704a80398ea
Verena Zaiser <development@verena-zaiser.de>
2023-06-04 15:57:13 +0200

    added tests

--- final_pummel_the_fish/test/screens/splash_screen_test.dart
+++ final_pummel_the_fish/test/screens/splash_screen_test.dart
@@ -1,7 +1,28 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:pummel_the_fish/screens/home_screen.dart';
+import 'package:pummel_the_fish/screens/splash_screen.dart';
 
 void main() {
-  testWidgets('splash screen ...', (tester) async {
-    // TODO: Implement test
+  testWidgets('SplashScreen', (tester) async {
+    await tester.pumpWidget(const MaterialApp(
+      home: SplashScreen(),
+    ));
+    await tester.pumpAndSettle();
+
+    expect(find.byType(Scaffold), findsOneWidget);
+    expect(find.byType(SafeArea), findsOneWidget);
+    expect(find.byType(Center), findsOneWidget);
+    expect(find.byType(Image), findsOneWidget);
+  });
+
+  testWidgets('SplashScreen changes to HomeScreen', (tester) async {
+    await tester.pumpWidget(const MaterialApp(
+      home: SplashScreen(),
+    ));
+    await tester.pumpAndSettle(const Duration(seconds: 3));
+    await tester.pumpAndSettle();
+
+    expect(find.byType(HomeScreen), findsOneWidget);
   });
 }
