dc8bf4f42b9ec224d1dfdc40567ea10f3df35670
Verena Zaiser <development@verena-zaiser.de>
2023-06-08 09:51:44 +0200

    added more tests

--- final_pummel_the_fish/test/data/repositories/firestore_pet_repository_test.dart
+++ final_pummel_the_fish/test/data/repositories/firestore_pet_repository_test.dart
@@ -82,17 +82,32 @@ void main() {
       height: 10.0,
       isFemale: true,
       owner: null,
-      id: 'xKQzJnetXmkOMYtQgKNm',
+      id: '1',
     );
 
-    await petRepository.addPet(pet, testDocId: 'xKQzJnetXmkOMYtQgKNm');
+    await petRepository.addPet(pet, testDocId: '1');
 
     final pets = await petRepository.getAllPets();
 
     expect(pets, contains(pet));
   });
 
-  /*test('updatePet updates a pet in the repository', () async {
+  test('addPet() without test document (coverage only)', () async {
+    const pet = Pet(
+      name: 'Test Pet',
+      species: Species.cat,
+      age: 2,
+      weight: 5.0,
+      height: 10.0,
+      isFemale: true,
+      owner: null,
+      id: '1',
+    );
+
+    await petRepository.addPet(pet);
+  });
+
+  test('updatePet updates a pet in the repository', () async {
     const pet = Pet(
       id: '1hdajhjkwad',
       name: 'Test Pet',
@@ -113,9 +128,9 @@ void main() {
     final pets = await petRepository.getAllPets();
 
     expect(pets, contains(updatedPet));
-  });*/
+  });
 
-  /*test('getPetById returns the correct pet', () async {
+  test('getPetById returns the correct pet', () async {
     const pet = Pet(
       id: '1',
       name: 'Test Pet',
@@ -132,9 +147,9 @@ void main() {
     final retrievedPet = await petRepository.getPetById(pet.id);
 
     expect(retrievedPet, equals(pet));
-  });*/
+  });
 
-  /*test('deletePetById deletes a pet from the repository', () async {
+  test('deletePetById deletes a pet from the repository', () async {
     const pet = Pet(
       id: '1',
       name: 'Test Pet',
@@ -153,5 +168,94 @@ void main() {
     final pets = await petRepository.getAllPets();
 
     expect(pets, isNot(contains(pet)));
-  });*/
+  });
+
+  test('getPetBySpecies returns the correct pets', () async {
+    const cat = Pet(
+      id: '1',
+      name: 'Test Pet',
+      species: Species.cat,
+      age: 2,
+      weight: 5.0,
+      height: 10.0,
+      isFemale: true,
+      owner: null,
+    );
+
+    const dog = Pet(
+      id: '2',
+      name: 'Test Pet',
+      species: Species.dog,
+      age: 2,
+      weight: 5.0,
+      height: 10.0,
+      isFemale: true,
+      owner: null,
+    );
+
+    const cat2 = Pet(
+      id: '3',
+      name: 'Test Pet',
+      species: Species.cat,
+      age: 2,
+      weight: 5.0,
+      height: 10.0,
+      isFemale: true,
+      owner: null,
+    );
+
+    await petRepository.addPet(cat, testDocId: cat.id);
+    await petRepository.addPet(dog, testDocId: dog.id);
+    await petRepository.addPet(cat2, testDocId: cat2.id);
+
+    final result = await petRepository.getPetBySpecies(Species.cat);
+
+    expect(result.length, 2);
+  });
+
+  test('getPetsOrderedByHeight returns the pets in correct order', () async {
+    const pet1 = Pet(
+      id: '1',
+      name: 'Test Pet',
+      species: Species.cat,
+      age: 2,
+      weight: 5.0,
+      height: 3,
+      isFemale: true,
+      owner: null,
+    );
+
+    const pet2 = Pet(
+      id: '2',
+      name: 'Test Pet',
+      species: Species.dog,
+      age: 2,
+      weight: 5.0,
+      height: 1,
+      isFemale: true,
+      owner: null,
+    );
+
+    const pet3 = Pet(
+      id: '3',
+      name: 'Test Pet',
+      species: Species.cat,
+      age: 2,
+      weight: 5.0,
+      height: 2,
+      isFemale: true,
+      owner: null,
+    );
+
+    await petRepository.addPet(pet1, testDocId: pet1.id);
+    await petRepository.addPet(pet2, testDocId: pet2.id);
+    await petRepository.addPet(pet3, testDocId: pet3.id);
+
+    final result = await petRepository.getPetsOrderedByHeight();
+
+    expect(result.length, 3);
+    expect(result[0], pet1);
+    expect(result[1], pet3);
+    expect(result[2], pet2);
+  });
 }
